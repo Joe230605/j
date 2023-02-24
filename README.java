4637d3423ca76bdd4c2afeef5880f0001e94b4cb
Joe230605 <126295041+Joe230605@users.noreply.github.com>
2023-02-24 17:28:33 -0500

    Update README.java

--- README.java
+++ README.java
@@ -1,1 +1,11 @@
-# j
+class noteensum{
+
+  public static int fixTeen(int n){
+      if(n>=13&&n<15&&n!=16&&n>=17&&n<=19);
+      return 0;
+    }
+    
+    public static void main(String[] args){
+      System.out.println(fixTeen(17));
+    }  
+}
